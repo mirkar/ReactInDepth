b69e8adba96f2b0e6a2a275144d75e68b2d231c5
Dmitriy Mirkarimov <mirkarimov@gmail.com>
2026-02-17 21:58:59 -0800

    chore: add junit xml

--- chapter10/menuitem/vite.config.js
+++ chapter10/menuitem/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: { globals: true, environment: "jsdom" },
+  test: { 
+    globals: true,
+    environment: "jsdom",
+  reporters: ['default', 'junit'], // 'default' for console output, 'junit' for XML
+  outputFile: 'junit.xml', // Output file for JUnit results
+  },
 })
